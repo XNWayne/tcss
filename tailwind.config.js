92aee41f601649ebee9de33f6038be07193fac46
Xander Wayne <xandernzaywa@gmail.com>
2022-05-10 05:10:31 +0300

    testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./src/.html"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        main: "#ffd100",
+        sec: "#0090e3",
+      },
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
